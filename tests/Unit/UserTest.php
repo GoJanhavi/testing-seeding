d69086e227cc2ad3156a669f83809a54edbbd64d
janhavi <jg688@njit.edu>
2018-10-30 17:29:54 -0400

    Task : Created a unit test to insert a defined user into the users table and confirm it's presence in database

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -19,4 +19,15 @@ class UserTest extends TestCase
         $user = factory(User::class)->create();
         $this->assertInstanceOf('App\User', $user);
     }
+    public function testCreateUserType2()
+    {
+        $user = factory(User::class)->create([
+            'name' => 'Janhavi',
+            'email' => 'jg688@njit.edu',
+            'email_verified_at' => now(),
+            'password' => '*#abcdef*',
+            'remember_token' => str_random(10),
+        ]);
+        $this->assertInstanceOf('App\User', $user);
+    }
 }
